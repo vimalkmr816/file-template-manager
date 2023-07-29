1a24a2cfd6a1e4100968c321652dbe46368187d8
Vimal Kumar <vimalkmr816@gmail.com>
2023-07-29 13:21:34 +0530

    added multiple file support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,318 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	"env" : {
+		"node"    : true,
+		"browser" : true,
+		"es2021"  : true
 	},
-	"extends": [
+	"extends" : [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended"
 	],
-	"overrides": [
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	"parser"        : "@typescript-eslint/parser",
+	"parserOptions" : {
+		"ecmaFeatures" : {
+			"jsx" : true
+		},
+		"ecmaVersion" : 12,
+		"sourceType"  : "module"
 	},
-	"plugins": [
+	"plugins" : [
+		"align-import",
+		"align-assignments",
 		"@typescript-eslint"
 	],
-	"rules": {
-		"indent": [
+	"settings" : {
+	},
+	"rules" : {
+		"indent" : [
 			"error",
-			"tab"
+			"tab",
+			{
+				"SwitchCase" : 1
+			}
 		],
-		"linebreak-style": [
+		// "@typescript-eslint/indent" : [ "error", 4 ],
+		"@typescript-eslint/member-delimiter-style" : [
+			"error",
+			{
+				"multiline" : {
+					"delimiter"   : "none",
+					"requireLast" : false
+				},
+				"singleline" : {
+					"delimiter"   : "comma",
+					"requireLast" : false
+				}
+			}
+		],
+		"@typescript-eslint/type-annotation-spacing" : [
+			"error",
+			{
+				"before"    : false,
+				"after"     : true,
+				"overrides" : {
+					"arrow" : {
+						"before" : true,
+						"after"  : true
+					}
+				}
+			}
+		],
+		"linebreak-style" : [
 			"error",
 			"unix"
 		],
-		"quotes": [
+		"quotes" : [
 			"error",
 			"double"
 		],
-		"semi": [
+		"semi" : [
+			"error",
+			"always"
+		],
+		"key-spacing" : [
+			"error",
+			{
+				"singleLine" : {
+					"beforeColon" : true,
+					"afterColon"  : true
+				},
+				"multiLine" : {
+					"beforeColon" : true,
+					"afterColon"  : true
+				},
+				"align" : {
+					"beforeColon" : true,
+					"afterColon"  : true,
+					"on"          : "colon"
+				}
+			}
+		],
+		"padding-line-between-statements" : [
+			"error",
+			{
+				blankLine : "always",
+				prev      : "*",
+				next      : "return"
+			},
+			{
+				blankLine : "always",
+				prev      : [
+					"case",
+					"default"
+				],
+				next : "*"
+			},
+			{
+				blankLine : "always",
+				prev      : "directive",
+				next      : "*"
+			},
+			{
+				blankLine : "any",
+				prev      : "directive",
+				next      : "directive"
+			},
+			{
+				blankLine : "always",
+				prev      : [
+					"const",
+					"let",
+					"var"
+				],
+				next : "*"
+			},
+			{
+				blankLine : "any",
+				prev      : [
+					"const",
+					"let",
+					"var"
+				],
+				next : [
+					"const",
+					"let",
+					"var"
+				]
+			}
+		],
+		"no-useless-escape" : [
+			1
+		],
+		"@typescript-eslint/no-empty-interface" : "warn",
+		"no-multiple-empty-lines"               : [
+			"error",
+			{
+				"max" : 1
+			}
+		],
+		"spaced-comment" : [
+			"error",
+			"always"
+		],
+		"arrow-parens" : [
+			"error",
+			"as-needed"
+		],
+		"max-len" : [
+			"off",
+			{
+				code : 160
+			}
+		],
+		/* "no-console" : [
+			2
+		], */
+		"block-spacing" : "error",
+		"brace-style"   : [
+			"error",
+			"1tbs",
+			{
+				"allowSingleLine" : false
+			}
+		],
+		"comma-dangle" : [
 			"error",
 			"never"
-		]
+		],
+		"comma-spacing" : [
+			"error",
+			{
+				"before" : false,
+				"after"  : true
+			}
+		],
+		"semi-spacing" : [
+			"error",
+			{
+				"before" : false,
+				"after"  : true
+			}
+		],
+		"semi-style" : [
+			"error",
+			"last"
+		],
+		"space-before-blocks"         : "error",
+		"space-before-function-paren" : [
+			"error",
+			"always"
+		],
+		"space-in-parens" : [
+			"error",
+			"always"
+		],
+		"func-call-spacing" : [
+			"error",
+			"always"
+		],
+		"switch-colon-spacing" : [
+			"error",
+			{
+				"before" : false,
+				"after"  : true
+			}
+		],
+		"align-import/align-import" : [
+			2
+		],
+		"align-import/trim-import" : [
+			2
+		],
+		"align-assignments/align-assignments" : [
+			2,
+			{
+				"requiresOnly" : false
+			}
+		],
+		"template-curly-spacing" : [
+			"error",
+			"always"
+		],
+		"no-trailing-spaces" : [
+			2
+		],
+		"no-whitespace-before-property" : "error",
+		"no-multi-spaces"               : [
+			"error",
+			{
+				"exceptions" : {
+					"ImportDeclaration"    : true,
+					"VariableDeclarator"   : true,
+					"AssignmentExpression" : true,
+					"JSXAttribute"         : true
+				}
+			}
+		],
+		"no-mixed-spaces-and-tabs" : [
+			2,
+			"smart-tabs"
+		],
+		"no-tabs" : [
+			2,
+			{
+				allowIndentationTabs : true
+			}
+		],
+		"object-property-newline" : [
+			"error",
+			{
+				"allowAllPropertiesOnSameLine" : true
+			}
+		],
+		"object-curly-newline" : [
+			"error",
+			{
+				"consistent" : true
+			}
+		],
+		"object-curly-spacing" : [
+			"error",
+			"always"
+		],
+		"array-bracket-spacing" : [
+			"error",
+			"always"
+		],
+		"arrow-spacing" : [
+			"error",
+			{
+				"before" : true,
+				"after"  : true
+			}
+		],
+		"keyword-spacing" : [
+			"error",
+			{
+				"before" : true,
+				"after"  : true
+			}
+		],
+		"jsx-quotes" : [
+			"error",
+			"prefer-double"
+		],
+		"space-infix-ops" : [
+			"error",
+			{
+				"int32Hint" : false
+			}
+		],
+		"@typescript-eslint/no-explicit-any" : "error",
+		"space-unary-ops"                    : [
+			2,
+			{
+				"words"     : true,
+				"nonwords"  : false,
+				"overrides" : {
+					"new" : false,
+					"++"  : true
+				}
+			}
+		],
+		"no-empty-function"                    : "off",
+		"@typescript-eslint/no-empty-function" : "warn",
+		"@typescript-eslint/no-unused-vars"    : "off"
 	}
 }
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,158 +1,196 @@
-import * as fs from "fs"
-import * as pathlib from "path"
-import * as vscode from "vscode"
+import * as fs      from "fs";
+import * as pathlib from "path";
+import * as vscode  from "vscode";
+import path = require( "path" );
 
-const excludedFiles = ["node_modules", ".git", ".next", ".env"]
+const excludedFiles      = [ "node_modules", ".git", ".next", ".env" ];
+const GENERIC_ERROR      = "Oops! Something went wrong. We're on it!";
+const INVALID_PATH_ERROR =  "Invalid input. Curly Braces only, please.";
 
-function showQuickPick (items: vscode.QuickPickItem[], onSelect: (v: readonly vscode.QuickPickItem[]) => void): void {
-	const quickPick = vscode.window.createQuickPick()
-	quickPick.items = items
+function showQuickPick ( items: vscode.QuickPickItem[], onSelect: ( v: readonly vscode.QuickPickItem[] ) => void ): void {
+	const quickPick = vscode.window.createQuickPick ();
 
-	quickPick.onDidChangeSelection((c) => {
-		onSelect(c)
-		quickPick.hide()
-	})
+	quickPick.items = items;
 
-	quickPick.onDidHide(() => { quickPick.dispose() })
+	quickPick.onDidChangeSelection ( c => {
+		onSelect ( c );
+		quickPick.hide ();
+	} );
 
-	quickPick.show()
+	quickPick.onDidHide ( () => {
+		quickPick.dispose ();
+	} );
+
+	quickPick.show ();
 }
 
-async function getAllFilesAndFoldersRecursively (folderPath: string | undefined, files: string[], folders: string[]): Promise<{ files: string[], folders: string[] } | undefined> {
-	if (folderPath !== undefined) {
-		const entries = fs.readdirSync(folderPath)
-		const filteredEntries = entries.filter((v) => !excludedFiles.includes(v))
+async function getAllFilesAndFoldersRecursively ( folderPath: string | undefined, files: string[], folders: string[] ): Promise<string[] | undefined> {
+	if ( folderPath !== undefined ) {
+		const entries         = fs.readdirSync ( folderPath );
+		const filteredEntries = entries.filter ( v => !excludedFiles.includes ( v ) );
 
-		filteredEntries.forEach((entry) => {
-			const fullPath = `${folderPath}/${entry}`
-			const stats = fs.statSync(fullPath)
-			const relativePath = vscode.workspace.asRelativePath(fullPath)
+		filteredEntries.forEach ( ( entry, index ) => {
+			const fullPath     = `${ folderPath }/${ entry }`;
+			const stats        = fs.statSync ( fullPath );
+			const relativePath = vscode.workspace.asRelativePath ( fullPath );
 
-			if (stats.isDirectory()) {
-				folders.push(relativePath)
-				void getAllFilesAndFoldersRecursively(fullPath, files, folders)
-			} else {
-				files.push(entry)
+			if ( stats.isDirectory () ) {
+				folders.push ( relativePath );
+				void getAllFilesAndFoldersRecursively ( fullPath, files, folders );
 			}
-		})
-		return { files, folders }
+		} );
+
+		return folders;
 	} else {
-		await vscode.window.showErrorMessage("No workspace folder is open.")
+		await vscode.window.showErrorMessage ( "No workspace folder is open." );
 	}
 }
 
+async function createFile ( filePath: string ) {
+	const isDirectory   = filePath.endsWith ( "/" );
+	const currentFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath; // Get the current workspace folder path
+	const isNested      = /(?!^)\/(?!$)/.test ( filePath );
 
+	if ( currentFolder ) {
+		const directoryPath        = pathlib.dirname ( filePath );
+		const currentDirectoryPath = pathlib.join ( currentFolder, directoryPath );
+		const currentFilePath      = pathlib.join ( currentFolder, filePath );
 
-function createFile(filePath: string) {
-	const isDirectory = filePath.endsWith("/")
-	const currentFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath // Get the current workspace folder path
-	const isNested = /(?!^)\/(?!$)/.test(filePath)
-
-
-	if(currentFolder){
-		const directoryPath = pathlib.dirname(filePath)
-		const currentDirectoryPath =pathlib.join(currentFolder,directoryPath) 
-		const currentFilePath = pathlib.join(currentFolder,filePath )
-		
-		if(isNested) { 
-			// nested directory 
+		if ( isNested ) {
+			// nested directory
 			// /pages/auth/
-			if (isDirectory) {
-				const currentDirectoryPath =pathlib.join(currentFolder,filePath) 
-				if(!fs.existsSync(directoryPath)) { 
-					fs.mkdirSync(currentDirectoryPath, { recursive: true })
-					console.log(`'${filePath}' created successfully!`)
-				}else {
-					console.log(`'${filePath}' already exists`)
+			if ( isDirectory ) {
+				const currentDirectoryPath = pathlib.join ( currentFolder, filePath );
+
+				if ( !fs.existsSync ( directoryPath ) ) {
+					fs.mkdirSync ( currentDirectoryPath, { recursive : true } );
+
+					await vscode.window.showInformationMessage ( `'${ filePath }' created successfully!` );
+				} else {
+					await vscode.window.showInformationMessage ( `'${ filePath }' already exists` );
 				}
 			} else {
-			// nested file 
+			// nested file
 			// /pages/auth/index.js
-				if(!fs.existsSync(directoryPath)) { 
-					fs.mkdirSync(currentDirectoryPath, { recursive: true })
-					fs.writeFileSync(currentFilePath, "")
+				if ( !fs.existsSync ( directoryPath ) ) {
+					fs.mkdirSync ( currentDirectoryPath, { recursive : true } );
+					fs.writeFileSync ( currentFilePath, "" );
 					try {
-						vscode.workspace.openTextDocument(currentFilePath)
-							.then((doc) => {
-								vscode.window.showTextDocument(doc)
-							})
-							.then( ()=> {
-								console.log(`File '${filePath}' created successfully!`)
-							})
-					} catch (error) {
-						console.log("========  error:", error)
+						vscode.workspace.openTextDocument ( currentFilePath )
+							.then ( doc => {
+								vscode.window.showTextDocument ( doc );
+							} )
+							.then ( async () => {
+								await vscode.window.showErrorMessage ( `'${ filePath }' created successfully!` );
+							} );
+					} catch ( error ) {
+						await vscode.window.showErrorMessage ( GENERIC_ERROR );
 					}
-				}else {
-					console.log(`'${filePath}' already exists`)
+				} else {
+					await vscode.window.showErrorMessage ( GENERIC_ERROR );
 				}
-			} 
-		}
-		else if (isDirectory) {
-
-			// root directory 
-			// /pages/
-			const currentDirectoryPath =pathlib.join(currentFolder,filePath) 
-			if(!fs.existsSync(filePath)) { 
-				console.log("========  filePath:", filePath)
-				fs.mkdirSync(currentDirectoryPath, { recursive: true })
-				console.log(`'${filePath}' created successfully!`)
-			}else {
-				console.log(`'${filePath}' already exists`)
 			}
-		}
-		else { 
-			// root file 
+		} else if ( isDirectory ) {
+
+			// root directory
+			// /pages/
+			const currentDirectoryPath = pathlib.join ( currentFolder, filePath );
+
+			if ( !fs.existsSync ( filePath ) ) {
+				fs.mkdirSync ( currentDirectoryPath, { recursive : true } );
+				await vscode.window.showErrorMessage ( `'${ filePath }' created successfully!` );
+			} else {
+				await vscode.window.showErrorMessage ( `'${ filePath }' already exists` );
+			}
+		} else {
+			// root file
 			// /index.ts
-			console.log("========  currentFilePath:", currentFilePath)
 			try {
-				fs.writeFileSync(currentFilePath, "")
-				vscode.workspace.openTextDocument(currentFilePath)
-					.then((doc) => {
-						vscode.window.showTextDocument(doc)
-					})
-					.then( ()=> {
-						console.log(`File '${filePath}' created successfully!`)
-					})
-			} catch (error) {
-				console.log("========  error:", error)
+				fs.writeFileSync ( currentFilePath, "" );
+				vscode.workspace.openTextDocument ( currentFilePath )
+					.then ( doc => {
+						vscode.window.showTextDocument ( doc );
+					} )
+					.then ( async () => {
+						await vscode.window.showErrorMessage ( GENERIC_ERROR );
+					} );
+			} catch ( error ) {
+				await vscode.window.showErrorMessage ( GENERIC_ERROR );
 			}
 		}
 	}
 }
+const splitInput = ( str: string ) => {
+	const input         = str.split ( "/" )?.at ( -1 )?.split ( "}" );
+	const cleanedString = input?.[0].replace ( /{|}|\[|\]/g, "" );
+	const filesArr      = cleanedString?.split ( "," ).map ( v => v.trim () );
 
-export function activate (context: vscode.ExtensionContext): void {
-	const disposable = vscode.commands.registerCommand("file-template-manager.helloWorld", async () => {
-		const currentFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath // Get the current workspace folder path
+	const b = filesArr?.map ( a => {
+		if ( a.split ( "." ).length <= 1 )
+			return a + input?.[1];
 
-		const files: string[] = []
-		const folders: string[] = []
-		const data = await getAllFilesAndFoldersRecursively(currentFolder, files, folders)
+		return a;
+	} );
 
-		const a = data?.folders.map((v) => {
-			return {
-				label: `/${ v }`
+	return { folder : str.split ( "/" ).slice ( 0, -1 ).join ( "/" ), filesArr : b };
+};
+
+export function activate ( context: vscode.ExtensionContext ): void {
+	const disposable = vscode.commands.registerCommand ( "file-template-manager.helloWorld", async () => {
+		const currentFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath; // Get the current workspace folder path
+
+		const files: string[]   = [];
+		const folders: string[] = [];
+		const data              = await getAllFilesAndFoldersRecursively ( currentFolder, files, folders );
+
+		const quickPickOptions = data?.map ( ( v, index ) => {
+			if ( index === 0 ) {
+				return {
+					label : "/"
+				};
 			}
-		})
 
-		a?.unshift({ label: "/"  })
+			return {
+				label : `/${ v }`
+			};
+		} );
 
-		if (a != null) {
-			showQuickPick(a, selection => {
+		if ( quickPickOptions != null ) {
+			showQuickPick ( quickPickOptions, selection => {
 				const inputPrompt = {
-					prompt: "Enter the file name",
-					placeHolder: "newfile.txt"
-				}
-				vscode.window.showInputBox(inputPrompt)
-					.then((inputVal) => {
-						if(inputVal)
-							createFile(pathlib.join( selection[0].label ,  inputVal ))
-					})
-			})
-		}
-	})
+					prompt      : "Enter the file name",
+					placeHolder : "newfile.txt"
+				};
 
-	context.subscriptions.push(disposable)
+				vscode.window.showInputBox ( inputPrompt )
+					.then ( generateFile ( selection ) );
+			} );
+		}
+	} );
+
+	context.subscriptions.push ( disposable );
 }
 
+function generateFile ( selection: readonly vscode.QuickPickItem[] ): ( ( value: string | undefined ) => void | Thenable<void> ) | undefined {
+	return async inputVal => {
+		if ( inputVal ) {
+			if ( inputVal.includes ( "{" ) ) {
+				if ( !inputVal.includes ( "}" ) ) {
+					await vscode.window.showErrorMessage ( INVALID_PATH_ERROR );
+
+					return;
+				} else {
+					const split = splitInput ( inputVal );
+
+					split.filesArr?.forEach ( val => {
+						createFile ( pathlib.join ( selection[0].label, split.folder, val ) );
+					} );
+				}
+			} else {
+				createFile ( pathlib.join ( selection[0].label, inputVal ) );
+			}
+		}
+	};
+}
 // This method is called when your extension is deactivated
 // export function deactivate () { }
